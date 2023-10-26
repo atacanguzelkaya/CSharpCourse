85d733314183c6004ba85dc56aaad4c947e7c1a8
atacanguzelkaya <atacangzlky@gmail.com>
2023-10-26 13:05:05 +0300

    Add Classes,  Add RecapDemo1

---
+++ Classes/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        //field bir classın içinde  özellikleri tutmak için kullanılanılır.
+        //public string FirstName; // bu şekilde de yazılabilir.
+
+        public int Id { get; set; }
+
+        // ---  ENCAPSULATION ---
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return "Mrs." + _firstName; }
+
+            set { _firstName = value; } // value set etmek istediğimiz değere karşılık gelir
+
+        }
+
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Interfaces
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            PersonManager manager = new PersonManager();
+            //manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName="Demiroğ", Address ="Ankara"});
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+            };
+
+            Student student = new Student
+            {
+                Id = 2,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Departmant = "Computer Sciences"
+            };
+
+            manager.Add(customer);
+
+            manager.Add(student);
+
+            Console.ReadLine();
+        }
+    }
+    interface IPerson  // Interface - Temel nesnedir.  Soyut nesnedir.
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+
+        }
+    }
+}
