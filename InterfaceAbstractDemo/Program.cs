0ec59409d9254e248096a0b755aa8ff96cb3943b
Atacan <Atacan@ATA>
2023-11-30 02:15:13 +0300

    Added InterfaceAbstractDemo

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1997, 7, 29), FirstName = "Atacan",LastName="Güzelkaya",NationalityId = "12341234123"});
+            Console.ReadLine();
+        }
+    }
+}
