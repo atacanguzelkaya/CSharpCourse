12ba7a9736373284c548fafa42b2222ac6b20f92
atacanguzelkaya <atacangzlky@gmail.com>
2024-01-05 12:39:16 +0300

    Added Generics, Interfaces, LinqProject

---
+++ Generics/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> sehirler = new List<string>();
+        sehirler.Add("ankara");
+        sehirler.Add("samsun");
+        sehirler.Add("istanbul");
+        sehirler.Add("izmir");
+        Console.WriteLine(sehirler.Count);  // Count burada property'dir.
+
+
+        MyList<string> sehirler2 = new MyList<string>();
+        sehirler2.Add("ankara");
+        sehirler2.Add("manisa");
+        sehirler2.Add("aydın");
+        sehirler2.Add("muğla");
+        sehirler2.Add("trabzon");
+        Console.WriteLine(sehirler2.Count); // buradaki Count aşağıda bizim kendi ürettiğimiz property'dir
+
+        MyList<int> rakamlar = new MyList<int>();
+        rakamlar.Add(1965);
+    }
+}
+
+
+class MyList<T> // Generic Class
+{
+    T[] _array;
+    T[] _tempArray;
+
+    public MyList()
+    {
+        _array = new T[0];
+    }
+    public void Add(T item)
+    {
+        _tempArray = _array;
+        _array = new T[_array.Length + 1]; 
+        for (int i = 0; i < _tempArray.Length; i++)
+        {
+            _array[i] = _tempArray[i];
+        }
+
+        _array[_array.Length - 1] = item; // burası index gösterir.
+    }
+
+    public int Count
+    {
+        get { return _array.Length; }
+    }
+}
---
+++ InterfacesCSharpSeries/Program.cs
@@ -0,0 +1,80 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //interfaceler newlenemez!
+        //IPersonManager customerManager = new CustomerManager();
+        //customerManager.Add();
+
+        //IPersonManager employeeManager = new EmployeeManager();
+        //employeeManager.Add();
+
+        ProjectManager projectManager = new ProjectManager();
+        //projectManager.Add(customerManager);
+        projectManager.Add(new InternManager());
+    }
+}
+
+// class PersonManager
+// {
+//     //implemented operation
+//     public void Add()
+//     {
+//         Console.WriteLine("Eklendi");
+//     }
+// }
+interface IPersonManager
+{
+    //unimplemented operation
+    void Add();
+    void Update();
+
+}
+//inherits- class , implements- interface
+//javada interface implements, class extends
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        //müşteri ekleme kodları
+        Console.WriteLine("Müşteri eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Müşteri güncellendi");
+    }
+}
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        //personel ekleme kodları
+        Console.WriteLine("Personel eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Personel güncellendi");
+    }
+}
+
+class InternManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Stajyer eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Stajyer güncellendi");
+    }
+}
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+}
